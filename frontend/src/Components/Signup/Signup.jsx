79965e6a11b9e8498948bc30ecd2f0308e369b64
Carle Medina <22-06230@g.batstate-u.edu.ph>
2024-10-10 19:32:32 +0800

    Node modules

--- frontend/src/Components/Signup/Signup.jsx
+++ frontend/src/Components/Signup/Signup.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react';
 import AuthButton from "../../UI/AuthButton";
 import AuthInput from "../../UI/AuthInput";
 
 const Signup = () => {
+    const [users, setUsers] = useState([]);
+    const [name, setName] = useState();
+    const [age, setAge] = useState();
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/getUsers')
+        .then((users) => {
+            setUsers(users.data)
+        }).catch(err => console.log(err));
+    }, []);
+
+    const Submit =() => {
+        axios.post('http://localhost:3000/createUser', {name, age})
+        .then((users) => {
+            console.log(users);
+        }).catch(err => console.log(err));
+    }
+
     return (
         <div className="form-container signup">
             <form>
@@ -12,7 +31,7 @@ const Signup = () => {
                 <AuthInput type={"password"} placeholder={"Password"}/>
                 <AuthInput type={"password"} placeholder={"Confirm Password"}/>
 
-                <AuthButton buttonText={"SIGN UP"}/>
+                <AuthButton buttonText={"SIGN UP"} onClick={Submit}/>
             </form>
         </div>
     )
