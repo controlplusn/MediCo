ce72161fec87e58d14da08565d02e29b26ee7ad7
Carle Medina <22-06230@g.batstate-u.edu.ph>
2024-10-11 16:11:24 +0800

    Delet folders

--- frontend/src/UI/AuthInput.jsx
+++
@@ -1,7 +0,0 @@
-const AuthInput = ({ type, placeholder, onChange }) => {
-    return (
-        <input type={type} placeholder={placeholder} onChange={onChange}></input>
-    )
-}
-
-export default AuthInput;
