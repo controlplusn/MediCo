5733db7ffbe873ada09e817f8f281c78615a76aa
Theaaaaaaa <theasalva19@gmail.com>
2024-11-05 19:59:55 +0800

    Dashboard UI

---
+++ MediSinag/src/components/Content.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ContentHeader from './ContentHeader';
+import Greetings from './Greetings';
+import Card from './Card';
+import StudyList from './StudyList';
+import '../assets/styles/content.css';
+
+const Content = () => {
+    return (
+    <div className="content">
+        <ContentHeader />
+        <Greetings />
+        <Card />
+        <StudyList />
+    </div>
+    );
+};
+
+export default Content;
