761166fa8875e14626669d7087f2ab7ca9e60903
Carle Medina <22-06230@g.batstate-u.edu.ph>
2024-11-06 21:18:17 +0800

    Dashboard: Fix dashboard

--- frontend/src/Components/Dashboard/Dashboard.jsx
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import Sidebar from './Sidebar'
 import Profile from './Profile'
 import Content from './Content'
+import '../../styles/dashboard.css';
 
 const Dashboard = () => {
   return (
-    <div>
-        <Sidebar />
-        <Content />
-        <Profile />
+    <div className="dashboard-container">
+        <div className='dashboard'>
+            <Sidebar />
+            <div className="dashboard--content">
+                <Content />
+                <Profile />
+        </div>
+    </div>
     </div>
   )
 }
 
 export default Dashboard
+
