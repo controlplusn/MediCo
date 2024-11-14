02ad786349f91f9453ccd203fe40b708e2ae8175
VexIvannnnnnn <sumangvex@gmail.com>
2024-11-14 12:46:59 +0800

    backend foundation

---
+++ community/src/backend/model/postModel.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+  Content: {
+    type: String,
+    required: true
+  },
+  PostedAt: {
+    type: Date,
+    default: Date.now // Will automatically set the current date and time if not provided
+  },
+  Subject: {
+    type: String,
+    required: true
+  },
+  commentId: {
+    type: mongoose.Schema.Types.ObjectId, 
+    auto: true
+  },
+  heartId: {
+    type: mongoose.Schema.Types.ObjectId, 
+    auto: true 
+  },
+  username: {
+    type: String,
+    ref: 'users', 
+    required: true
+  }
+});
+
+const Post = mongoose.model('posts', postSchema);
+
+export default Post;
---
+++ community/src/backend/post.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import Post from './model/postModel.js'; 
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = 6969;
+
+const uri = 'mongodb+srv://admin:adminuser123@cluster0.4408g.mongodb.net/MediCo?retryWrites=true&w=majority';
+
+mongoose.connect(uri)
+.then(() => {
+    console.log("Connected to DB")
+    app.listen(PORT,() => console.log("Server is Running"))})
+.catch((err) => console.log(err));
+
+
+app.post("/add", async (req, res) => {
+    try {
+      const { Content, Subject, username } = req.body;
+  
+      // Validate the required fields
+      if (!Content || !Subject || !username) {
+        return res.status(400).json({ error: 'Content, Subject, and Username are required.' });
+      }
+  
+      // Create a new Post document
+      const newPost = new Post({
+        Content,
+        Subject,
+        username
+      });
+  
+      // Save the new post to the database
+      const savedPost = await newPost.save();
+  
+      // Send the saved post as a response
+      res.status(201).json(savedPost);
+    } catch (e) { // Correctly referencing the error variable
+      console.error('Error adding data:', e);
+      res.status(500).send({ success: false, message: "Error adding data" });
+    }
+  });
