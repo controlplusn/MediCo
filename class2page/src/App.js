9ba97d75865af7a3760a32141a1e2afb78246732
VexIvannnnnnn <sumangvex@gmail.com>
2024-11-22 23:19:10 +0800

    done

--- class2page/src/App.js
+++ class2page/src/App.js
@@ -4,7 +4,7 @@ import Class2 from './components/Class2.jsx'
 export const App = () => {
   return (
     <div>
-      <Class2 username={'carlemedina'} classId={'6740393d1c872f6e61d40684'}/>
+      <Class2 username={'carlemedina'} classId={'67409fb002476bcf7239c21c'}/>
     </div>
   )
 }
