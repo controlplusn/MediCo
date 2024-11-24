e8d8173a8b5f10e0f9abfdffe8a72edc527a6c14
Carle Medina <22-06230@g.batstate-u.edu.ph>
2024-11-24 21:35:20 +0800

    Class feature

---
+++ frontend/src/Components/Class/Class.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ClassHeader from './ClassHeader.jsx';
+import ClassContent  from './ClassContent.jsx';
+import '../styles/class.css'
+
+export const Class = () => {
+  return (
+    <div className="class-container">
+        <ClassHeader />
+        <ClassContent />
+    </div>
+  )
+}
+
+export default Class;
