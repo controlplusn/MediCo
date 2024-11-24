4e50c961f49358dba5568a61d5a6ddc57ba821a9
Carle Medina <22-06230@g.batstate-u.edu.ph>
2024-11-24 21:38:50 +0800

    Add styles in class component

--- frontend/src/Components/Class/Class.jsx
+++ frontend/src/Components/Class/Class.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ClassHeader from './ClassHeader.jsx';
 import ClassContent  from './ClassContent.jsx';
-import '../styles/class.css'
-
+import '../../styles/class.css';
 export const Class = () => {
   return (
     <div className="class-container">
