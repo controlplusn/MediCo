726a372f4a52e0e1e69f50c2d6298a60b063bd75
Carle Medina <22-06230@g.batstate-u.edu.ph>
2024-11-27 20:07:24 +0800

    Add sidebar on class component

--- frontend/src/Components/Class/Class.jsx
+++ frontend/src/Components/Class/Class.jsx
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react'
 import ClassHeader from './ClassHeader';
 import ClassContent  from './ClassContent';
 import '../../styles/class.css';
+import Sidebar from '../Dashboard/Sidebar';
 
 const Class = () => {
   return (
-    <div className="class-container">
+    <div className="class-page-container">
+      <Sidebar />
+
+      <div className="class-container">
         <ClassHeader />
         <ClassContent />
+      </div>
     </div>
   )
 }
