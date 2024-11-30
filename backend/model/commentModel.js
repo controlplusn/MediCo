9b0e6607a84b666d06d53a3c3165754ff85390c1
VexIvannnnnnn <sumangvex@gmail.com>
2024-11-30 12:18:21 +0800

    comment in community

--- backend/model/commentModel.js
+++ backend/model/commentModel.js
@@ -11,6 +11,10 @@ const commentSchema = new mongoose.Schema({
     ref: 'User',
     required: true
   },
+  username:{
+    type:String,
+    required:true
+  },
   commentId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'posts',
