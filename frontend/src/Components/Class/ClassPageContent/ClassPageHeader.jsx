5df6ee09c432bfc3a2bb57386bc060a8bfa0824b
VexIvannnnnnn <sumangvex@gmail.com>
2024-12-05 05:34:39 +0800

    class nav

--- frontend/src/Components/Class/ClassPageContent/ClassPageHeader.jsx
+++ frontend/src/Components/Class/ClassPageContent/ClassPageHeader.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../../../styles/classpageheader.css'
 import {Icon} from '@iconify/react'
+import { Link } from 'react-router-dom'
 
 export const ClassPageHeader = () => {
   return (
     <div className="headerclass">
+      <Link to="/class">
         <Icon icon="weui:back-outlined"/>
+      </Link>
         <h5>Class</h5>
     </div>
             
